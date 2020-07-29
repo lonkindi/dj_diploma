d90f61254ebab8d910c9f376cc4ef8ddc0c1db35
lonkindi <legions.ru@gmail.com>
2020-07-29 15:44:40 +0500

    commit from job 29072020

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import logout, authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.core.paginator import Paginator
 
 from app.forms import LoginForm
 from app.models import Section, Product
@@ -28,10 +29,8 @@ def login_view(request):
                 return redirect(main_view)
             else:
                 pass
-                # Return a 'disabled account' error message
         else:
             return HttpResponse('Invalid login or passpword!')
-    # Return an 'invalid login' error message.
     template = 'app/registration/login.html'
     form = LoginForm()
     context = {'form': form}
@@ -44,28 +43,44 @@ def logout_view(request):
     return render(request, template_name=template, context=context)
 
 def section_view(request, id=0):
-    sections = '',
-    product_list = '',
-    current_section = ''
+    prod_page = ''
+    current_page = ''
+    prev_page, next_page = '', ''
+    current_section = 0
+    sections_page = ''
     template = 'app/empty_section.html'
     sections = Section.objects.all()
     if id:
         try:
             current_section = sections.filter(id=id)[0]
+            sections_page = current_section.get_children()
         except IndexError:
             current_section = 0
         if current_section:
-            current_id = id
-            template = 'app/section.html'
-
-            product_list = Product.objects.filter(section=current_id)
-
-
-            # print('len current_section=', len(current_section))
-
+            if len(sections_page) == 0:
+                current_id = id
+                template = 'app/section.html'
+                prod_list = Product.objects.filter(section=current_id)
+                paginator = Paginator(prod_list, 2)
+                current_page = request.GET.get('page', 1)
+                prod_page = paginator.get_page(current_page)
+                if prod_page.has_previous():
+                    prev_page = prod_page.previous_page_number()
+                else:
+                    prev_page = 1
+                if prod_page.has_next():
+                    next_page = prod_page.next_page_number()
+                else:
+                    next_page = paginator.num_pages
+    if not current_section:
+        current_section = 'Добро пожаловать в Простомагазин!'
     context = {'sections': sections,
-               'product_list': product_list,
                'current_section': current_section,
+               'section_page': sections_page,
+               'prod_page': prod_page,
+               'current_page': current_page,
+               'prev_page_url': f'{reverse(section_view)}{id}?page={prev_page}',
+               'next_page_url': f'{reverse(section_view)}{id}?page={next_page}',
                }
     return render(request, template_name=template, context=context)
 
